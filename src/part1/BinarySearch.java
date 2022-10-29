57f3cc1df0455e18aed368faaac9668d8fed8236
= <caleb@stcroixwebagency.com>
2022-10-29 12:07:29 -0500

    added recursive BinarySearch method

--- src/part1/BinarySearch.java
+++ src/part1/BinarySearch.java
@@ -1,12 +1,12 @@
 package part1;
 
 public class BinarySearch {
-	
+
 	public static int binarySearch(int[] a, int key) {
 		int low = 0;
-		int high = a.length-1;
+		int high = a.length - 1;
 		while (low <= high) {
-			int mid = low + (high - low)/2;
+			int mid = low + (high - low) / 2;
 			if (key < a[mid]) {
 				high = mid - 1;
 			} else if (key > a[mid]) {
@@ -15,6 +15,24 @@ public class BinarySearch {
 				return mid;
 		}
 		return -1;
+	}
+
+	// recursive
+	public static int binarySearch(int[] a, int key, int low, int high) {
+		// find median id
+		int mid = low + (high - low) / 2;
+
+		if (key == a[mid])
+			return mid;
+
+		if (key < a[mid]) {
+			binarySearch(a, key, low, mid);
+		} else if (key > a[mid]) {
+			binarySearch(a, key, mid, high);
+		}
+
+		return -1;
+
 	}
 
 }
